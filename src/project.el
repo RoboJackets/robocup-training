643842c414774315ad613aa97123aac7bc19b048
Jay Kamat <jaygkamat@gmail.com>
2016-08-24 22:11:30 -0400

    Add test files!

---
+++ src/project.el
@@ -0,0 +1,18 @@
+
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+  '(("rc-slides"
+      :base-directory "."
+      :publishing-directory "../slides/"
+      :publishing-function org-reveal-publish-to-reveal
+      :exclude-tags ("docs"))
+     ("rc-docs"
+      :base-directory "."
+      :publishing-directory "../docs/"
+      :publishing-function org-md-publish-to-md
+      )))
+
+(defun my-org-publish ()
+  (interactive)
+  (org-publish-all))
