08e684a6ac08b4832497f7447b75ed15399763b0
Jay Kamat <jaygkamat@gmail.com>
2016-09-14 16:20:21 -0400

    Make training slides project have proper file basedir

--- src/project.el
+++ src/project.el
@@ -1,18 +1,19 @@
 
 (require 'ox-publish)
 
-(setq org-publish-project-alist
-  '(("rc-slides"
-      :base-directory "."
-      :publishing-directory "../slides/"
-      :publishing-function org-reveal-publish-to-reveal
-      :exclude-tags ("docs"))
-     ("rc-docs"
-       :base-directory "."
-       :publishing-directory "../docs/"
-       :publishing-function org-gfm-publish-to-gfm
-       :exclude-tags ("slides")
-       )))
+(let ((proj-base (file-name-directory load-file-name)))
+  (setq org-publish-project-alist
+    `(("rc-slides"
+        :base-directory ,(concat proj-base ".")
+        :publishing-directory ,(concat proj-base "../slides/")
+        :publishing-function org-reveal-publish-to-reveal
+        :exclude-tags ("docs"))
+       ("rc-docs"
+         :base-directory ,(concat proj-base ".")
+         :publishing-directory ,(concat proj-base "../docs/")
+         :publishing-function org-gfm-publish-to-gfm
+         :exclude-tags ("slides")
+         ))))
 
 (defun my-org-publish ()
   (interactive)
